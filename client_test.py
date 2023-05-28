3d48dd055f95fbe1386486d6d0b310526752137c
ikechukwueze <themedric@live.com>
2023-05-28 13:24:12 +0100

    test getDataPoint for case where bid price is equal to ask price

--- client_test.py
+++ client_test.py
@@ -54,6 +54,24 @@ class ClientTest(unittest.TestCase):
 
       # assert returned value matches expected value
       self.assertEqual(getDataPoint(quote), expected_return_value)
+  
+  
+  def test_getDataPoint_calculatePriceAskEqualToBid(self):
+    quotes = [
+      {'top_ask': {'price': 120.48, 'size': 109}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 36}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 81}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 121.68, 'size': 4}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      # compute expected outcomes
+      expected_stock = quote['stock']
+      expected_bid_price = float(quote['top_bid']['price'])
+      expected_ask_price = float(quote['top_ask']['price'])
+      expected_price = (expected_bid_price + expected_ask_price)/2
+      expected_return_value = (expected_stock, expected_bid_price, expected_ask_price, expected_price)
+
+      # assert returned value matches expected value
+      self.assertEqual(getDataPoint(quote), expected_return_value)
 
 
 
